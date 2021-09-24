a1a41761362e3c161fecaf5ea30cf7c1ccd18072
David Abadi <GL.DAVID.GL@GMAIL.COM>
2021-09-24 14:45:25 -0400

    intial commit

---
+++ public/alldata.js
@@ -0,0 +1,20 @@
+function AllData(){
+    const [data, setData] = React.useState('');    
+
+    React.useEffect(() => {
+        
+        // fetch all accounts from API
+        fetch('/account/all')
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                setData(JSON.stringify(data));                
+            });
+
+    }, []);
+
+    return (<>
+        <h5>All Data in Store:</h5>
+        {data}
+    </>);
+}
