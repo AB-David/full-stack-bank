afdb1fa5ba4d6d4f29f0f6fb94838940d990fa5d
David Abadi <GL.DAVID.GL@GMAIL.COM>
2021-09-25 16:05:54 -0400

    added login

--- public/index.js
+++ public/index.js
@@ -1,14 +1,25 @@
 function Spa() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false); 
   return (
     <HashRouter>
       <div>
         <NavBar/>        
-        <UserContext.Provider value={{users:[{name:'abel',email:'abel@mit.edu',password:'secret',balance:100}]}}>
+        <UserContext.Provider value={
+          {users:[{name:'abel',email:'abel@mit.edu',password:'secret',balance:100}],
+          isLoggedIn:isLoggedIn,
+          setIsLoggedIn:setIsLoggedIn}}>
           <div className="container" style={{padding: "20px"}}>
             <Route path="/" exact component={Home} />
             <Route path="/CreateAccount/" component={CreateAccount} />
             <Route path="/login/" component={Login} />
-            <Route path="/deposit/" component={Deposit} />
+            {isLoggedIn ? (  
+              <>
+                <Route path="/deposit/" component={Deposit} />
+              </>
+                ):(
+              <>
+              </>
+                )}
             <Route path="/withdraw/" component={Withdraw} />
             {/* <Route path="/transactions/" component={Transactions} /> */}
             <Route path="/balance/" component={Balance} />
